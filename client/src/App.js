a8cbe9ee85bfbae8969261045137ccff8c2f23ec
Ragib Sina <ragib.sina@gmail.com>
2022-09-06 16:53:14 -0600

    added footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,18 @@
-
-
-import './App.scss';
-import { BrowserRouter, Routes, Route, useParams } from 'react-router-dom';
+import "./App.scss";
+import { BrowserRouter, Routes, Route, useParams } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 function App() {
-
-
   return (
     <div className="App">
       <BrowserRouter>
         <Header />
         <Routes>
-          <Route path='/warehouses' element={<Warehouse />}></Route>
-          <Route path='/inventory' element={<Inventory />}></Route>
+          <Route path="/warehouses" element={<Warehouse />}></Route>
+          <Route path="/inventory" element={<Inventory />}></Route>
         </Routes>
+        <Footer />
       </BrowserRouter>
     </div>
   );
