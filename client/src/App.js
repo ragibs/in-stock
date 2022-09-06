d30fa89c3f1c31d86897345c42043006d8ce68ba
John Bosley <johnbosley@Johns-MacBook-Air.local>
2022-09-06 18:01:43 -0400

    added routers to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,22 @@
-import "./App.scss";
+
+
+import './App.scss';
+import { BrowserRouter, Routes, Route, useParams } from 'react-router-dom';
 
 function App() {
-  return <div className="App"></div>;
+
+
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path='/warehouses' element={<Warehouse />}></Route>
+          <Route path='/inventory' element={<Inventory />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
