15a24b65d657172a7652cce2bd155c1c6ac897c0
Michelle Rogers <michelleevarogers@gmail.com>
2022-09-07 13:45:10 -0700

    implementing validation

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import "./App.scss";
 import { BrowserRouter, Routes, Route, useParams } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
-import Header from "./components/Header/Header";
 
 function App() {
   return (
