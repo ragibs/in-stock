89f75edf45b178a1b18ecc492b40d3f2d92f7b91
Michelle Rogers <michelleevarogers@gmail.com>
2022-09-06 20:44:09 -0700

    add state, change and submit functions

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,6 @@ import Header from "./components/Header/Header";
 
 function App() {
   return (
-
     <div className="App">
       <BrowserRouter>
         <Header />
@@ -17,7 +16,6 @@ function App() {
         <Footer />
       </BrowserRouter>
     </div>
-
   );
 }
 
