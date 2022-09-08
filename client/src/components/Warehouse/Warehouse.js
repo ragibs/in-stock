a72353fdd9892a4e5abfde0d2bff928be8bd60ca
Ragib Sina <ragib.sina@gmail.com>
2022-09-08 17:37:19 -0600

    upto date with develop

--- client/src/components/Warehouse/Warehouse.js
+++ client/src/components/Warehouse/Warehouse.js
@@ -7,7 +7,7 @@ import rightArrow from "../../assets/Icons/chevron_right-24px.svg";
 import DeleteModal from "../DeleteModal/DeleteModal";
 import sortArrow from "../../assets/Icons/sort-24px.svg";
 
-function Warehouse({ warehousesData }) {
+function Warehouse(props) {
   const [deleteModal, setDeleteModal] = useState(false); //state to control delete modal
   const [deleteWarehouse, setDeleteWarehouse] = useState([""]); //to pass id to deletemodal to send API delete request
 
@@ -16,82 +16,87 @@ function Warehouse({ warehousesData }) {
     setDeleteWarehouse([name, id]);
     setDeleteModal(true);
   };
+
   return (
-    <>
-      <div className="warehouse__top">
-        <h1 className="warehouse__title">Warehouses</h1>
-        <div className="warehouse__topright">
-          <input
-            type="text"
-            className="warehouse__search"
-            placeholder="Search..."
-          ></input>
-          <button className="warehouse__button">+ Add New Warehouse</button>
-        </div>
-      </div>{" "}
-      {deleteModal && (
-        <DeleteModal
-          setDeleteModal={setDeleteModal}
-          deleteWarehouse={deleteWarehouse}
-        />
-      )}
+    <div>
       {/* if deleteModal is set to true, the modal will render */}
-      <div className="warehouse__categories">
-        <div className="warehouse__categoryAndArrow">
-          <span className="warehouse__category">WAREHOUSE</span>
-          <img src={sortArrow} className="warehouse__sortArrow" />
+      <div className="warehouse__container">
+        {deleteModal && (
+          <DeleteModal
+            setDeleteModal={setDeleteModal}
+            deleteWarehouse={deleteWarehouse}
+          />
+        )}
+        <div className="warehouse__top">
+          <h1 className="warehouse__title">Warehouses</h1>
+          <div className="warehouse__topright">
+            <input
+              type="text"
+              className="warehouse__search"
+              placeholder="Search..."
+            ></input>
+            <button className="warehouse__button">+ Add New Warehouse</button>
+          </div>
         </div>
-        <div className="warehouse__categoryAndArrow">
-          <span className="warehouse__category">ADDRESS</span>
-          <img src={sortArrow} className="warehouse__sortArrow" />
+
+        <div className="warehouse__categories">
+          <div className="warehouse__categoryAndArrow">
+            <span className="warehouse__category">WAREHOUSE</span>
+            <img src={sortArrow} className="warehouse__sortArrow" />
+          </div>
+          <div className="warehouse__categoryAndArrow">
+            <span className="warehouse__category">ADDRESS</span>
+            <img src={sortArrow} className="warehouse__sortArrow" />
+          </div>
+          <div className="warehouse__categoryAndArrow">
+            <span className="warehouse__category">CONTACT NAME</span>
+            <img src={sortArrow} className="warehouse__sortArrow" />
+          </div>
+          <div className="warehouse__categoryAndArrow">
+            <span className="warehouse__category">CONTACT INFORMATION</span>
+            <img src={sortArrow} className="warehouse__sortArrow" />
+          </div>
+          <span className="warehouse__category--right">ACTIONS</span>
         </div>
-        <div className="warehouse__categoryAndArrow">
-          <span className="warehouse__category">CONTACT NAME</span>
-          <img src={sortArrow} className="warehouse__sortArrow" />
-        </div>
-        <div className="warehouse__categoryAndArrow">
-          <span className="warehouse__category">CONTACT INFORMATION</span>
-          <img src={sortArrow} className="warehouse__sortArrow" />
-        </div>
-        <span className="warehouse__category--right">ACTIONS</span>
+
+        {props.warehousesData.map((warehouse) => (
+          <div className="warehouse" key={warehouse.id}>
+            <div className="warehouse__text">
+              <div className="warehouse__left">
+                <Link
+                  to={`/warehouses/${warehouse.id}`}
+                  className="warehouse__link"
+                >
+                  <div className="warehouse__nameAndArrow">
+                    <p className="warehouse__name">{warehouse.name}</p>
+
+                    <img src={rightArrow} className="warehouse__rightArrow" />
+                  </div>
+                </Link>
+                <p className="warehouse__address">
+                  {warehouse.address}, {warehouse.city}, {warehouse.country}
+                </p>
+              </div>
+              <div className="warehouse__right">
+                <p className="warehouse__contact">{warehouse.contact.name}</p>
+                <p className="warehouse__contactinfo">
+                  <span>{warehouse.contact.phone}</span>
+                  <span>{warehouse.contact.email}</span>
+                </p>
+              </div>
+            </div>
+            <div className="warehouse__icons">
+              <img
+                onClick={() => delHandle(warehouse.name, warehouse.id)} //passing both name and id of selected wh
+                src={deleteIcon}
+                className="warehouse__deleteicon"
+              />
+              <img src={editIcon} className="warehouse__editicon" />
+            </div>
+          </div>
+        ))}
       </div>
-      {warehousesData.map((warehouse) => (
-        <div className="warehouse" key={warehouse.id}>
-          <div className="warehouse__text">
-            <div className="warehouse__left">
-              <Link
-                to={`/warehouses/${warehouse.id}`}
-                className="warehouse__link"
-              >
-                <div className="warehouse__nameAndArrow">
-                  <p className="warehouse__name">{warehouse.name}</p>
-                  <img src={rightArrow} className="warehouse__rightArrow" />
-                </div>
-              </Link>
-              <p className="warehouse__address">
-                {warehouse.address}, {warehouse.city}, {warehouse.country}
-              </p>
-            </div>
-            <div className="warehouse__right">
-              <p className="warehouse__contact">{warehouse.contact.name}</p>
-              <p className="warehouse__contactinfo">
-                {warehouse.contact.phone}
-                <p>{warehouse.contact.email}</p>
-              </p>
-            </div>
-          </div>
-          <div className="warehouse__icons">
-            <img
-              onClick={() => delHandle(warehouse.name, warehouse.id)} //passing both name and id of selected wh
-              src={deleteIcon}
-              className="warehouse__deleteicon"
-            />
-            <img src={editIcon} className="warehouse__editicon" />
-          </div>
-        </div>
-      ))}
-    </>
+    </div>
   );
 }
-
 export default Warehouse;
