92695c980f86388e6c5bdbeeeae6c44fe2a3391e
Ragib Sina <ragib.sina@gmail.com>
2022-09-07 19:33:54 -0600

    font end complete, back end work left

--- client/src/components/Warehouse/Warehouse.js
+++ client/src/components/Warehouse/Warehouse.js
@@ -1,46 +1,74 @@
-import './Warehouse.scss';
-import { Link } from 'react-router-dom';
-import deleteIcon from "../../assets/Icons/delete_outline-24px.svg"
-import editIcon from "../../assets/Icons/edit-24px.svg"
-import rightArrow from "../../assets/Icons/chevron_right-24px.svg"
+import "./Warehouse.scss";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import deleteIcon from "../../assets/Icons/delete_outline-24px.svg";
+import editIcon from "../../assets/Icons/edit-24px.svg";
+import rightArrow from "../../assets/Icons/chevron_right-24px.svg";
+import DeleteModal from "../DeleteModal/DeleteModal";
 
-function Warehouse(props) {     
-    return (   
-        <>
-        <div className="warehouse__top">
-            <h1 className="warehouse__title">Warehouses</h1>
-            <div className="warehouse__topright">
-                <input type="text" className="warehouse__search" placeholder='Search...'></input>
-                <button className="warehouse__button">+ Add New Warehouse</button>
-            </div>
-        </div>       
-        {props.warehousesData.map((thumb) =>
-            <div className="warehouse" key={thumb.id}>
-                <div className="warehouse__text">
-                    <div className="warehouse__left">
-                        <Link to={`/${thumb.id}`} className="warehouse__link">
-                            <div className="warehouse__nameAndArrow">
-                                <p className="warehouse__name">{thumb.name}</p>
-                                <img src={rightArrow} className="warehouse__rightArrow"/>
-                            </div>                            
-                        </Link>
-                        <p className="warehouse__address">{thumb.address}, {thumb.city}, {thumb.country}</p>
-                    </div>
-                    <div className="warehouse__right">
-                        <p className="warehouse__contact">{thumb.contact.name}</p>
-                        <p className="warehouse__contactinfo">{thumb.contact.phone}<p>{thumb.contact.email}</p></p>
-                    </div>
+function Warehouse({ warehousesData }) {
+  const [deleteModal, setDeleteModal] = useState(false); //state to control delete modal
+  const [deleteWarehouse, setDeleteWarehouse] = useState([""]); //to pass id to deletemodal to send API delete request
+
+  //delete request handler
+  const delHandle = (name, id) => {
+    setDeleteWarehouse([name, id]);
+    setDeleteModal(true);
+  };
+  return (
+    <>
+      <div className="warehouse__top">
+        <h1 className="warehouse__title">Warehouses</h1>
+        <div className="warehouse__topright">
+          <input
+            type="text"
+            className="warehouse__search"
+            placeholder="Search..."
+          ></input>
+          <button className="warehouse__button">+ Add New Warehouse</button>
+        </div>
+      </div>
+      {deleteModal && (
+        <DeleteModal
+          setDeleteModal={setDeleteModal}
+          deleteWarehouse={deleteWarehouse}
+        />
+      )}
+      {/* if deleteModal is set to true, the modal will render */}
+      {warehousesData.map((warehouse) => (
+        <div className="warehouse" key={warehouse.id}>
+          <div className="warehouse__text">
+            <div className="warehouse__left">
+              <Link to={`/${warehouse.id}`} className="warehouse__link">
+                <div className="warehouse__nameAndArrow">
+                  <p className="warehouse__name">{warehouse.name}</p>
+                  <img src={rightArrow} className="warehouse__rightArrow" />
                 </div>
-                <div className="warehouse__icons">
-                    <img src={deleteIcon} className="warehouse__deleteicon"/>
-                    <img src={editIcon} className="warehouse__editicon"/>
-                </div>                
-            </div>              
-        )}
-        </>        
-    );
+              </Link>
+              <p className="warehouse__address">
+                {warehouse.address}, {warehouse.city}, {warehouse.country}
+              </p>
+            </div>
+            <div className="warehouse__right">
+              <p className="warehouse__contact">{warehouse.contact.name}</p>
+              <p className="warehouse__contactinfo">
+                {warehouse.contact.phone}
+                <p>{warehouse.contact.email}</p>
+              </p>
+            </div>
+          </div>
+          <div className="warehouse__icons">
+            <img
+              onClick={() => delHandle(warehouse.name, warehouse.id)} //passing both name and id of selected wh
+              src={deleteIcon}
+              className="warehouse__deleteicon"
+            />
+            <img src={editIcon} className="warehouse__editicon" />
+          </div>
+        </div>
+      ))}
+    </>
+  );
 }
 
-export default Warehouse
-
-
+export default Warehouse;
--- server/routes/warehousesRoutes.js
+++ server/routes/warehousesRoutes.js
@@ -4,16 +4,16 @@ const fs = require("fs");
 const cors = require("cors");
 app.use(cors());
 const router = express.Router();
-app.use(express.json());  
+app.use(express.json());
 
 const readFile = (fileName) => {
-    const fileContent = JSON.parse(fs.readFileSync(`./data/${fileName}.json`));
-    return fileContent;
+  const fileContent = JSON.parse(fs.readFileSync(`./data/${fileName}.json`));
+  return fileContent;
 };
 
 router.get("/", function (req, res) {
-    const warehouses = readFile("warehouses");
-    res.json(warehouses);
+  const warehouses = readFile("warehouses");
+  res.json(warehouses);
 });
 
 module.exports = router;
