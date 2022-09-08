c916b8cb89d31354ff9cbf108da73a09d8637f4c
John Bosley <johnbosley@Johns-MacBook-Air.local>
2022-09-08 17:27:55 -0400

    finished warehouse inventory

--- client/src/components/Warehouse/Warehouse.js
+++ client/src/components/Warehouse/Warehouse.js
@@ -4,40 +4,40 @@ import deleteIcon from "../../assets/Icons/delete_outline-24px.svg"
 import editIcon from "../../assets/Icons/edit-24px.svg"
 import rightArrow from "../../assets/Icons/chevron_right-24px.svg"
 
-function Warehouse(props) {     
-    return (   
+function Warehouse(props) {
+    return (
         <>
-        <div className="warehouse__top">
-            <h1 className="warehouse__title">Warehouses</h1>
-            <div className="warehouse__topright">
-                <input type="text" className="warehouse__search" placeholder='Search...'></input>
-                <button className="warehouse__button">+ Add New Warehouse</button>
+            <div className="warehouse__top">
+                <h1 className="warehouse__title">Warehouses</h1>
+                <div className="warehouse__topright">
+                    <input type="text" className="warehouse__search" placeholder='Search...'></input>
+                    <button className="warehouse__button">+ Add New Warehouse</button>
+                </div>
             </div>
-        </div>       
-        {props.warehousesData.map((thumb) =>
-            <div className="warehouse" key={thumb.id}>
-                <div className="warehouse__text">
-                    <div className="warehouse__left">
-                        <Link to={`/${thumb.id}`} className="warehouse__link">
-                            <div className="warehouse__nameAndArrow">
-                                <p className="warehouse__name">{thumb.name}</p>
-                                <img src={rightArrow} className="warehouse__rightArrow"/>
-                            </div>                            
-                        </Link>
-                        <p className="warehouse__address">{thumb.address}, {thumb.city}, {thumb.country}</p>
+            {props.warehousesData.map((thumb) =>
+                <div className="warehouse" key={thumb.id}>
+                    <div className="warehouse__text">
+                        <div className="warehouse__left">
+                            <Link to={`/warehouses/${thumb.id}`} className="warehouse__link">
+                                <div className="warehouse__nameAndArrow">
+                                    <p className="warehouse__name">{thumb.name}</p>
+                                    <img src={rightArrow} className="warehouse__rightArrow" />
+                                </div>
+                            </Link>
+                            <p className="warehouse__address">{thumb.address}, {thumb.city}, {thumb.country}</p>
+                        </div>
+                        <div className="warehouse__right">
+                            <p className="warehouse__contact">{thumb.contact.name}</p>
+                            <p className="warehouse__contactinfo">{thumb.contact.phone}<p>{thumb.contact.email}</p></p>
+                        </div>
                     </div>
-                    <div className="warehouse__right">
-                        <p className="warehouse__contact">{thumb.contact.name}</p>
-                        <p className="warehouse__contactinfo">{thumb.contact.phone}<p>{thumb.contact.email}</p></p>
+                    <div className="warehouse__icons">
+                        <img src={deleteIcon} className="warehouse__deleteicon" />
+                        <img src={editIcon} className="warehouse__editicon" />
                     </div>
                 </div>
-                <div className="warehouse__icons">
-                    <img src={deleteIcon} className="warehouse__deleteicon"/>
-                    <img src={editIcon} className="warehouse__editicon"/>
-                </div>                
-            </div>              
-        )}
-        </>        
+            )}
+        </>
     );
 }
 
