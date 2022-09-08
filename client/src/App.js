1e92332d71b44a0713dfacb0a90af7c861065300
Ragib Sina <ragib.sina@gmail.com>
2022-09-07 19:33:54 -0600

    font end complete, back end work left

--- client/src/App.js
+++ client/src/App.js
@@ -4,22 +4,18 @@ import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import HomePage from "./pages/HomePage/HomePage";
 
-
-
 function App() {
   return (
-
     <div className="App">
       <BrowserRouter>
         <Header />
         <Routes>
           <Route path="/" element={<HomePage />}></Route>
-          <Route path="/inventory" element={<Inventory />}></Route>
+          {/* <Route path="/inventory" element={<Inventory />}></Route> */}
         </Routes>
         <Footer />
       </BrowserRouter>
     </div>
-
   );
 }
 
--- client/src/components/DeleteModal/DeleteModal.js
+++ client/src/components/DeleteModal/DeleteModal.js
@@ -1,19 +1,35 @@
 import "./DeleteModal.scss";
 import axios from "axios";
+import { useEffect } from "react";
 
-function DeleteModal() {
+function DeleteModal({ setDeleteModal, deleteWarehouse }) {
+  const cancelHandler = () => {
+    //if we click on X or cancel, the modal will close
+    setDeleteModal(false);
+  };
+  const deleteHandler = () => {
+    console.log(deleteWarehouse[0], deleteWarehouse[1]);
+    axios
+      .delete(`http://localhost:8080/warhouses/${deleteWarehouse[1]}`)
+      .then((response) => alert("Warehouse delete successful"))
+      .catch((err) => console.error(err));
+  };
   return (
     <div className="deleteModal-bg">
       <div className="deleteModal-container">
-        <button onClick={cancelHandler} className="deleteModal__close-btn">
-          X
-        </button>
-        <div className="deleteModal__text-container">
-          <h1 className="deleteModal__title">Delete Washington warehouse?</h1>
-          <p className="deleteModal__text">
-            Please confirm that you’d like to delete the Washington from the
-            list of warehouses. You won’t be able to undo this action.
-          </p>
+        <div className="deleteModal__top-container">
+          <button onClick={cancelHandler} className="deleteModal__close-btn">
+            X
+          </button>
+          <div className="deleteModal__text-container">
+            <h1 className="deleteModal__title">
+              Delete {deleteWarehouse[0]} warehouse?
+            </h1>
+            <p className="deleteModal__text">
+              Please confirm that you’d like to delete the Washington from the
+              list of warehouses. You won’t be able to undo this action.
+            </p>
+          </div>
         </div>
         <div className="deleteModal__btn-container">
           <button
