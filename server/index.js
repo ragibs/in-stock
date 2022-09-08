b8e254e22ee4a84fc27ef3a69f29fbc699f5ad7a
John Bosley <johnbosley@Johns-MacBook-Air.local>
2022-09-08 17:35:36 -0400

    with server

--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ const { PORT, BACKEND_URL } = process.env;
 const inventories = require('./data/inventories.json')
 const warehouses = require('./data/warehouses.json')
 const warehousesRoutes = require("./routes/warehousesRoutes");
+const inventoryRoutes = require("./routes/inventoryRoutes");
 
 //cors middleware
 app.use(cors());
@@ -14,9 +15,11 @@ app.use(cors());
 //middleware to give us access to req.body
 app.use(express.json());
 
-app.get('/inventories', (req, res) => {
-  res.send(inventories)
-})
+
+app.use("/warehouses", warehousesRoutes);
+
+app.use("/inventories", inventoryRoutes);
+
 
 app.get('/warehouses', (req, res) => {
   res.send(warehouses)
@@ -26,6 +29,8 @@ app.get('/warehouses/:id', (req, res) => {
   res.send(warehouses.find((obj) => {
     return obj.id === `${req.params.id}`
   }))
+
+
 })
 
 
--- server/routes/warehousesRoutes.js
+++ server/routes/warehousesRoutes.js
@@ -4,16 +4,29 @@ const fs = require("fs");
 const cors = require("cors");
 app.use(cors());
 const router = express.Router();
-app.use(express.json());  
+app.use(express.json());
+
+
 
 const readFile = (fileName) => {
     const fileContent = JSON.parse(fs.readFileSync(`./data/${fileName}.json`));
     return fileContent;
 };
 
+const warehouseData = JSON.parse(fs.readFileSync(`./data/warehouses.json`));
 router.get("/", function (req, res) {
     const warehouses = readFile("warehouses");
     res.json(warehouses);
+});
+
+
+router.get("/:id", (req, res) => {
+    let warehouseID = warehouseData.find((warehouse) => warehouse.id === req.params.id);
+    if (warehouseID) {
+        res.json(warehouseID);
+    } else {
+        res.status(404).send(" requested warehouse not found");
+    }
 });
 
 module.exports = router;
