f2279eb0d5733d2639b7d44fb9e623bee6c06a3a
Shaurrya <shaurrya@hotmail.com>
2022-09-08 01:23:01 -0400

    started making inventory page

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route, useParams } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import HomePage from "./pages/HomePage/HomePage";
+import InventoryPage from "./pages/InventoryPage/InventoryPage";
 import NewWarehouse from "./components/NewWarehouse/NewWarehouse";
 
 function App() {
@@ -12,7 +13,7 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<HomePage />}></Route>
-          <Route path="/inventory" element={<Inventory />}></Route>
+          <Route path="/inventory" element={<InventoryPage />}></Route>
           <Route path="/new-warehouse" element={<NewWarehouse />}></Route>
         </Routes>
         <Footer />
