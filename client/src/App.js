f0601f773c68eba97223f82fcc2d9ff446eacf07
Michelle Rogers <michelleevarogers@gmail.com>
2022-09-09 15:35:24 -0700

    got routes working

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,11 @@
 import "./App.scss";
-import { BrowserRouter, Routes, Route, useParams } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  useParams,
+  Navigate,
+} from "react-router-dom";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import HomePage from "./pages/HomePage/HomePage";
@@ -18,13 +24,15 @@ function App() {
       <BrowserRouter>
         <Header />
         <Routes>
-          <Route path="/" element={<HomePage />}></Route>
-
+          <Route path="/" element={<Navigate to="/warehouses" />}></Route>
+          <Route path="/warehouses" element={<HomePage />}></Route>
           <Route path="/inventory" element={<InventoryPage />}></Route>
           <Route
             path="/inventory/:id"
             element={<InventoryItemsDetails />}
           ></Route>
+
+          <Route path="/inventory/new" element={<NewItem />} />
 
           <Route
             path="/warehouses/:warehouseID"
@@ -32,7 +40,6 @@ function App() {
           ></Route>
 
           <Route path="/warehouses/new" element={<NewWarehouse />}></Route>
-          <Route path="/inventories/new" element={<NewItem />}></Route>
         </Routes>
         <Footer />
       </BrowserRouter>
