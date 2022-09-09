ebeff29f1461543c841c2da7464654c8624f005d
Michelle Rogers <michelleevarogers@gmail.com>
2022-09-09 13:53:20 -0700

    add validation to add item page

--- client/src/components/Warehouse/Warehouse.js
+++ client/src/components/Warehouse/Warehouse.js
@@ -1,5 +1,5 @@
 import "./Warehouse.scss";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import { useState } from "react";
 import deleteIcon from "../../assets/Icons/delete_outline-24px.svg";
 import editIcon from "../../assets/Icons/edit-24px.svg";
@@ -26,9 +26,11 @@ function Warehouse({ warehousesData }) {
             className="warehouse__search"
             placeholder="Search..."
           ></input>
-          <button className="warehouse__button">+ Add New Warehouse</button>
+          <NavLink to="/new-warehouse" className="warehouse__button">
+            + Add New Warehouse
+          </NavLink>
         </div>
-      </div>{" "}
+      </div>
       {deleteModal && (
         <DeleteModal
           setDeleteModal={setDeleteModal}
