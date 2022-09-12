13fb953b02797d3b6eed64b1fc38801e42376245
TaliaFontaine <taliafontaine1@gmail.com>
2022-09-11 17:42:41 -0700

    add router get and put requests in warehouseroutes

--- server/routes/warehousesRoutes.js
+++ server/routes/warehousesRoutes.js
@@ -95,4 +95,31 @@ router.post("/new", (req, res) => {
   }
 });
 
+//route to get warehouseId
+router.get("/:id", (req, res) => {
+  let warehouseID = warehouseData.find((warehouse) => warehouse.id === req.params.id);
+  if (warehouseID) {
+    res.json(warehouseID);
+  } else {
+    res.status(404).send(" requested warehouse not found");
+  }
+});
+  
+  
+//update a single warehouse
+router.put("/:id", (req, res) => {
+  let id = req.params.id;
+  let body = req.body;
+  let index = warehouseData.findIndex((warehouse) => warehouse.id === id);
+  if (index >= 0) {
+    let updatedWarehouse = { id: id, ...body };
+    fs.writeFileSync("./data/warehouses.json", JSON.stringify(warehouseData));
+    warehouseData[index] = updatedWarehouse;
+    res.send(updatedWarehouse);
+  } else {
+    res.status(404).send("Warehouse was not found");
+  }
+});
+
+
 module.exports = router;
